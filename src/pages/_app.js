45eeffb6580172f59d12bf63840af7ee1988f504
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-08-21 09:49:14 +0700

    Update analytics on _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
+import { useRouter } from "next/router";
 import Cursor from "../components/Cursor";
 import ScrollToTop from "../components/Scroll-to-top";
 import LoadingScreen from "../components/Loading-Screen";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-4E9XJ16RTJ', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <title>RantAI</title>
         <link rel="icon" href="/img/favicon-rantai.ico" />
       </Head>
+
+      {/* Google Tag Manager - Global site tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4E9XJ16RTJ"
+      />
+      <Script
+        strategy="afterInteractive"
+        id="google-analytics"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4E9XJ16RTJ');
+        `}
+      </Script>
+
       <Cursor />
       <LoadingScreen />
       <ScrollToTop />
@@ -22,24 +56,26 @@ function MyApp({ Component, pageProps }) {
         strategy="beforeInteractive"
         id="wow"
         src="/js/wow.min.js"
-      >
-      </Script>
+      />
       <Script
         strategy="beforeInteractive"
         id="splitting"
         src="/js/splitting.min.js"
-      ></Script>
-      <Script id="simpleParallax" src="/js/simpleParallax.min.js"></Script>
+      />
+      <Script
+        id="simpleParallax"
+        src="/js/simpleParallax.min.js"
+      />
       <Script
         strategy="beforeInteractive"
         id="isotope"
         src="/js/isotope.pkgd.min.js"
-      ></Script>
+      />
       <Script
-        strategy="lazyOnload" 
-        id="initWow" 
+        strategy="lazyOnload"
+        id="initWow"
         src="/js/initWow.js"
-      ></Script>
+      />
     </>
   );
 }
