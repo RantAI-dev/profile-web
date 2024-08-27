13906e00915436b3365725756437e152f10bd2d1
JeyyGit <20415821+JeyyGit@users.noreply.github.com>
2024-08-27 13:27:20 +0700

    Fix apostrophe

--- src/components/Minimal-area/index.jsx
+++ src/components/Minimal-area/index.jsx
@@ -42,7 +42,7 @@ const MinimalArea = () => {
                 Agent-Based Modeling. Our proficiency in AI-driven coding and optimization 
                 allows us to deliver comprehensive, end-to-end scientific simulation and 
                 digital twin solutions. At RantAI, we are dedicated to pushing the boundaries 
-                of technology, delivering cutting-edge solutions to tackle the world's most 
+                of technology, delivering cutting-edge solutions to tackle the world&apos;s most 
                 critical scientific problems. RantAI is also as pioneer in open book publishing 
                 for scientific computing, setting the standard for technological innovation. 
                 We are going to use this scientific publication as our marketing arm to reach 
